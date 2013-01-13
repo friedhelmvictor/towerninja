7afcefa934a68b531b081208575e644c5096e812
fridodeluxe <friedrich.maiwald@gmx.de>
2013-01-13 18:43:38 +0100

    added first player tracking implementation

--- TowerNinja/src/net/codegames/towerninja/Main.java
+++ TowerNinja/src/net/codegames/towerninja/Main.java
@@ -10,7 +10,12 @@ public class Main extends PApplet {
 	 * Object should take care of things.
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final int width = 640;
+	private static final int height = 480;
 	private Game game;
+	private Players players;
+	private boolean devMode = true;
+	
 
 	/**
 	 * Initial setup of the Applet. Also creating the {@link Game} object
@@ -18,9 +23,12 @@ public class Main extends PApplet {
 	 * @see processing.core.PApplet#setup()
 	 */
 	public void setup() {
-		size(600, 800);
+		
+		size(width, height);
 		frameRate(30);
 		game = new Game();
+		//players = new Players(this);
+		
 	}
 
 	/**
@@ -31,6 +39,12 @@ public class Main extends PApplet {
 	public void draw() {
 		background(128);
 		game.update();
+		
+		if (devMode) {
+			fill(255);
+			noStroke();
+			text((int)frameRate, 10, 20);
+		}
 	}
 
 	public static void main(String[] args) {
